28f8dfc9e20e2ee2b859367b190e8c407b7ec5da
Ankit Gupta <ankit1381997@gmail.com>
2024-03-18 11:49:11 +0530

    remove Team title from menu section panel

--- components/Panel/team.js
+++ components/Panel/team.js
@@ -26,7 +26,7 @@ export default function Team({
               <path d="M13.707,6.707L23,16l-9.293,9.293C13.077,25.923,12,25.477,12,24.586V7.414C12,6.523,13.077,6.077,13.707,6.707z"></path>
             </svg>
           </div>
-          <h2
+          {/* <h2
             style={{
               margin: "0",
               border: "0",
@@ -40,7 +40,7 @@ export default function Team({
             }}
             className={`${styles["TypographyPresentation--h6"]} ${styles.TypographyPresentation} ${styles["TypographyPresentation--overflowTruncate"]} ${styles["TypographyPresentation--fontWeightMedium"]} ${styles["TypographyPresentation--colorNavigation"]}`}>
             Team
-          </h2>
+          </h2> */}
         </div>
       </div>
       <div
